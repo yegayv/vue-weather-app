3aa45147d27368e3518070d295b14723888ea5bb
yegayv <yegay@sheridancollege.ca>
2023-07-14 18:57:02 -0400

    Implemented the search function, connected Mapbox API

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="flex flex-col min-h-screen font-Roboto">
+  <div class="flex flex-col min-h-screen font-Roboto bg-weather-primary">
     <SiteNavigation />
     <RouterView />
   </div>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,9 +1,36 @@
-<script setup>
-
-</script>
-
 <template>
-  <main>
-
+  <main class="container text-white">
+    <div class="pt-4 mb-8 relative">
+      <input type="text" v-model="searchQuery" @input="getSearchResults" placeholder="Search for a city or state"
+        class="py-2 px-1 w-full bg-transparent border-b focus:border-weather-secondary focus:outline-none focus:shadow-[0px_1px_0_0_#004E71]">
+      <ul class="absolute bg-weather-secondary text-white w-full shadow-md py-2 px-1 top-[66px]"
+        v-if="mapboxSearchResults">
+        <li v-for="searchResult in mapboxSearchResults" :key="searchResult.id" class="py-2 cursor-pointer">
+          {{ searchResult.place_name }}
+        </li>
+      </ul>
+    </div>
   </main>
 </template>
+
+<script setup>
+import { ref } from "vue";
+import axios from "axios";
+
+const mapboxAPIKey = "pk.eyJ1IjoieWVnYXl2IiwiYSI6ImNsazM0dmF5ZTA0c3MzbHMxNm44MWR6ZnEifQ.0Lyr3xd0m-6ZCY1fZ2gkqw"
+const searchQuery = ref("");
+const queryTimeout = ref(null);
+const mapboxSearchResults = ref(null);
+
+const getSearchResults = () => {
+  clearTimeout(queryTimeout.value);
+  queryTimeout.value = setTimeout(async () => {
+    if (searchQuery.value !== "") {
+      const result = await axios.get(`https://api.mapbox.com/geocoding/v5/mapbox.places/${searchQuery.value}.json?access_token=${mapboxAPIKey}&types=place`);
+      mapboxSearchResults.value = result.data.features;
+      return;
+    }
+    mapboxSearchResults.value = null;
+  }, 300);
+}
+</script>
