355711ffd2ec3b7fd07c30c1510a6c68e32189b5
yegayv <yegay@sheridancollege.ca>
2023-07-15 13:03:04 -0400

    Added feels like field, weather descritpion, weather icon. Added bottom scrollbar showing hourly weather

--- src/components/AsyncCityView.vue
+++ src/components/AsyncCityView.vue
@@ -1,8 +1,10 @@
 <template>
   <div class="flex flex-col flex-1 items-center">
+
     <div v-if="route.query.preview" class="text-white p-4 bg-weather-secondary w-full text-center">
       <p>You are currently previewing this city, click the "+" icon to start tracking this city.</p>
     </div>
+
     <div class="flex flex-col items-center text-white py-12">
       <h1 class="text-4xl mb-2">{{ route.params.city }}</h1>
       <p class="text-sm mb-12">
@@ -29,6 +31,39 @@
       <p class="text-8xl mb-8">
         {{ Math.round(weatherData.current.temp) }}&deg;
       </p>
+      <p>
+        Feels like
+        {{ Math.round(weatherData.current.feels_like) }} &deg;
+      </p>
+      <p class="capitalize">
+        {{ weatherData.current.weather[0].description }}
+      </p>
+      <img :src="`http://openweathermap.org/img/wn/${weatherData.current.weather[0].icon}@2x.png`" alt=""
+        class="w-[150px] h-auto">
+    </div>
+
+    <hr class="border-white border-opacity-10 w-full border">
+
+    <div class="max-w-screen-md w-full py-12">
+      <div class="mx-8 text-white">
+        <h2 class="mb-4">Hourly Weather</h2>
+        <div class="flex gap-10 overflow-x-scroll">
+          <div v-for="hourData in weatherData.hourly" :key="hourData.dt" class="flex flex-col gap-4 items-center">
+            <p class="whitespace-nowrap text-md">
+              {{
+                new Date(hourData.currentTime).toLocaleTimeString("en-us", {
+                  hour: "numeric",
+                })
+              }}
+            </p>
+            <img :src="`http://openweathermap.org/img/wn/${weatherData.current.weather[0].icon}@2x.png`" alt=""
+              class="h-[50px] w-auto object-cover">
+            <p class="text-xl">
+              {{ Math.round(hourData.temp) }} &deg;
+            </p>
+          </div>
+        </div>
+      </div>
     </div>
   </div>
 </template>
